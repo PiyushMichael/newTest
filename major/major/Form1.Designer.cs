5703c5eb75871612350f4e863ca91cb9f13da3e4
PiyushMichael <PiyushMichael@users.noreply.github.com>
2019-06-09 00:02:46 +0530

    folder add test

---
+++ major/major/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace major
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(229, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "match";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 144);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(14, 65);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "folder";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 185);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(292, 141);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(105, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(105, 67);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(199, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 341);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ major/major/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace major
+{
+    public partial class Form1 : Form
+    {
+        Image i1, i2;
+        Bitmap b1, b2;
+        shape ab, bc;
+        String[] lst;
+        float[] matches;
+        float max=0;
+        int index;
+        public class shape
+        {
+            public int pixels, size, width, height;
+            public int left, right, up, down;
+            public int first_x, first_y;
+            public int last_x, last_y;
+            public float diag_slope, diag_length;
+            public List<int> x, y;
+            public int[,] match_matrix;
+            private System.Drawing.Bitmap b1;
+
+            public shape(Bitmap bm)
+            {
+                Color cp;
+                pixels = 0;
+                width = bm.Width - 1;
+                height = bm.Height - 1;
+                size = bm.Height * bm.Width - 1;
+                x = new List<int>();
+                y = new List<int>();
+                for (int j = 0; j < height; j++)
+                    for (int i = 0; i < width; i++)
+                    {
+                        cp = bm.GetPixel(i, j);
+                        if (cp.R * cp.G * cp.B == 0)
+                        {
+                            pixels++;
+                            x.Add(i);
+                            y.Add(j);
+                        }
+                    }
+            }
+            public void measure()
+            {
+                first_x = x.First();
+                first_y = y.First();
+                last_x = x.Last();
+                last_y = y.Last();
+                match_matrix = new int[width, height];
+                diag_slope = (float)(last_y - first_y) / (float)(last_x - first_x);
+                diag_length = (last_y - first_y) * (last_y - first_y) - (last_x - first_x) * (last_x - first_x);
+                diag_length = (float)Math.Sqrt(diag_length);
+                left = x[0]; right = x[0]; up = y[0]; down = y[0];
+                for (int i = 1; i < pixels; i++)
+                {
+                    if (x[i] < left) left = x[i];
+                    if (x[i] > right) right = x[i];
+                    if (y[i] < up) up = y[i];
+                    if (y[i] > down) down = y[i];
+                }
+
+                for (int j = 0; j < height; j++)
+                    for (int i = 0; i < width; i++) match_matrix[i, j] = 0;
+                for (int i = 0; i < pixels; i++) if (x[i] < width && y[i] < height) match_matrix[x[i], y[i]] = 1;
+            }
+            public void translate(int a, int b)
+            {
+                for (int i = 0; i < pixels; i++)
+                {
+                    x[i] -= a;
+                    y[i] -= b;
+                }
+            }
+            public void fit()
+            {
+                int x_trans, y_trans;
+                x_trans = left - 5;
+                y_trans = up - 5;
+                translate(x_trans, y_trans);
+                down -= up - 5;
+                first_y -= up - 5;
+                last_y -= up - 5;
+                right -= left - 5;
+                first_x -= left - 5;
+                last_x -= left - 5;
+                up = left = 5;
+                width = right + 5;
+                height = down + 5;
+            }
+            public void scale(float num)
+            {
+                /*List<int> x1, y1;
+                x1 = new List<int>();
+                y1 = new List<int>();
+                left = (int)((float)left / num);
+                right = (int)((float)right / num);
+                up = (int)((float)up / num);
+                down = (int)((float)down / num);
+                first_x = (int)((float)first_x / num);
+                first_y = (int)((float)first_y / num);
+                last_x = (int)((float)last_x / num);
+                last_y = (int)((float)last_y / num);
+                diag_length /= num;
+                x1.Add((int)((float)x[0] / num));
+                y1.Add((int)((float)y[0] / num));*/
+                for (int i = 0; i < pixels; i++)
+                {
+                    x[i] = (int)((float)x[i] / num);
+                    y[i] = (int)((float)y[i] / num);
+                    /*if (x1.Last() != x[i] && y1.Last() != y[i])
+                    {
+                        x1.Add(x[i]);
+                        y1.Add(y[i]);
+                    }*/
+                }
+                //x = x1; y = y1;
+                //pixels = x1.Count();
+            }
+        }
+         public float match_shape(shape s1,shape s2)
+        {
+            //the last function
+            int[,] m;
+            int total_pixels, matched_pixels;
+            float match;
+            total_pixels = matched_pixels = 0;
+            s1.measure();
+            s2.measure();
+            s2.scale(s2.diag_length / s1.diag_length);
+            s2.measure();
+            s1.fit();
+            s2.fit();
+            s1.measure();
+            s2.measure();
+
+
+            int w = s1.width; if (s2.width < w) w = s2.width;
+            int h = s1.height; if (s2.height < h) h = s2.height;
+            m = new int[w, h];
+            for (int j = 0; j < h - 1; j++)
+            {
+                for (int i = 0; i < w - 1; i++)
+                {
+                    try
+                    {
+                        m[i, j] = s1.match_matrix[i, j] * s2.match_matrix[i, j];
+                    }
+                    catch (IndexOutOfRangeException e)
+                    {
+                        return 0;
+                    }
+                }
+            }
+                total_pixels = w * h;
+                for (int j = 0; j < h - 1; j++)
+                {
+                    for (int i = 0; i < w - 1; i++)
+                    {
+                        matched_pixels += m[i, j];
+                    }
+                }
+                match = (float)matched_pixels / (float)s1.pixels * 100;
+            return match;
+            //return 0;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            b1 = new Bitmap(openFileDialog1.FileName);
+            i1 = b1;
+            ab = new shape(b1);
+            ab.measure();
+            textBox2.Text = openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //openFileDialog1.ShowDialog();
+            //b2 = new Bitmap(openFileDialog1.FileName);
+            //i2 = b2;
+            //bc = new shape(b2);
+            //bc.measure();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //float x = match_shape(ab, bc);
+            ////richTextBox1.AppendText(Environment.NewLine);
+            //textBox1.Text = Convert.ToString(x);
+            for (int i = 0; i < lst.Count(); i++)
+            {
+                b2 = new Bitmap(lst[i]);
+                i2 = b2;
+                bc = new shape(b2);
+                bc.measure();
+                matches[i] = match_shape(ab, bc);
+                richTextBox1.AppendText(matches[i].ToString());
+                richTextBox1.AppendText(Environment.NewLine);
+            }
+            for (int i = 0; i < lst.Count(); i++)
+            {
+                if (matches[i] > max)
+                {
+                    max = matches[i];
+                    index = i;
+                }
+            }
+            textBox1.Text = lst[index];
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            String address;
+            address = folderBrowserDialog1.SelectedPath.ToString();
+            //textBox1.Text = address;
+            lst = System.IO.Directory.GetFiles(address);
+            matches = new float[lst.Count()];
+            for (int i = 0; i < lst.Count(); i++)
+            {
+                richTextBox1.AppendText(lst[i]);
+                richTextBox1.AppendText(Environment.NewLine);
+            }
+            textBox3.Text = address;
+        }
+    }
+}
