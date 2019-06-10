ba8a3fbdd63602ac85770f03b0fc4d34b0d8b0d9
PiyushMichael <PiyushMichael@users.noreply.github.com>
2019-06-10 11:09:53 +0530

    react file updated

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import Popup from 'reactjs-popup';
 
 const strElement = <div><h1>Hello React! H1 element added</h1> <li>peugeot</li> <li>citreon</li> <li>renault</li></div>
 
@@ -13,12 +14,24 @@ class Garage extends React.Component
 
 class Car extends React.Component
 {
-	constructor()
+	constructor(props)
 	{
-		super();
-		this.state = {bleh: "red", col: "EHEH"};
+		super(props);
+		this.state = {bleh: "red", col: "EHEH", model: props.col.model, show: true};
 		this.innerHTML = "inner HTML text :O"
+	};
+	changeColor =() =>{
+		if(this.state.bleh == "red")this.setState({bleh: "BLUE"});
+		if(this.state.bleh == "BLUE")this.setState({bleh: "red"});
 	}
+	delThis = () => {
+		this.setState({show: false});
+		alert("deleted everythign lol");
+	}
+	/*static getDerivedStateFromProps(props,state)
+	{
+		return {model: props.col.model};
+	}*/
 	render()
 	{
 		return (
@@ -28,8 +41,28 @@ class Car extends React.Component
 				<p>second para {this.innerHTML}</p>
 				<p>passing car details as a prop: {this.props.col.brand}, {this.props.col.model}</p>
 				<p>same variable name as prop but in state {this.state.col}</p>
+				<p>details through getDerivedStateFromProps: {this.state.bleh}, {this.state.col}, <b>{this.state.model}</b></p>
+				<br/>
+				<p>issa {this.state.bleh} color car</p>
+				<button type="button" onClick={this.changeColor}>change colour</button>
+				<button type="button" onClick={this.delThis}>delete everything</button>
+				<div id="boo"></div>
+				<div id="deb"></div>
+				<div id="up"></div>
 			</div>
 		);
+	}
+	componentDidMount(){
+		setTimeout(() => {this.setState({model: 2000})}, 2000); //setState gets overridden by getDerivedStateFromProps. pass props in constructor instead
+		document.getElementById("deb").innerHTML = "componentDidMount executed";
+	}
+	getSnapshotBeforeUpdate(prevProps,prevState)
+	{
+		document.getElementById("boo").innerHTML = "before the update value was: "+prevState.bleh;
+	}
+	componentDidUpdate()
+	{
+		document.getElementById("up").innerHTML += "component updated<br>"
 	}
 }
 class App extends React.Component
