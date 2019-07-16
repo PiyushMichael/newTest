9b6b7fc31591721171d894d2df69da09729c1a40
PiyushMichael <32812229+PiyushMichael@users.noreply.github.com>
2019-07-16 23:03:09 +0530

    Add files via upload

---
+++ major/Border/Border/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Border
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(391, 283);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 352);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(51, 18);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(409, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(492, 283);
+            this.panel1.TabIndex = 3;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(69, 352);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(51, 18);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(826, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "extract";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(357, 12);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.TickFrequency = 50;
+            this.trackBar1.Value = 50;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.update);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 381);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ major/Border/Border/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Border
+{
+    public partial class Form1 : Form
+    {
+        int extension_flag = 0;
+        Bitmap pic;
+        Image im;
+        Color pix;
+        Graphics gfx;
+        SolidBrush brush,brush1;
+        Pen penka,penka1;
+        Point p;
+        int threshold = 50, Difference_value = 0;
+        int width, height;
+        int[,] matrix_red,matrix_green,matrix_blue, border_matrix,fill_matrix1,fill_matrix2;
+        float x, y;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void bucketfill(int x, int y)
+        {
+            if (fill_matrix1[x, y] == 0) fill_matrix1[x, y] = 2;
+            else return;
+            if (x < width - 2) bucketfill(x + 1, y);
+            if (y < height - 2) bucketfill(x, y + 1);
+            if (x > 0) bucketfill(x - 1, y);
+            //if (y > 0) bucketfill(x, y - 1);
+        }
+        public void bucketfill_twin(int x, int y)
+        {
+            if (fill_matrix2[x, y] == 0) fill_matrix2[x, y] = 2;
+            else return;
+            if (x < width - 2) bucketfill_twin(x + 1, y);
+            if (y < height - 2) bucketfill_twin(x, y + 1);
+            if (x > 0) bucketfill_twin(x - 1, y);
+            if (y > 0) bucketfill_twin(x, y - 1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string name, extension;
+            name = openFileDialog1.SafeFileName.ToString();
+            int i = name.Length - 1;
+            while (!name[i].Equals('.'))
+            {
+                i--;
+            }
+            extension = name.Substring(i);
+            richTextBox1.Text = extension;
+            if ((extension.Equals(".jpeg")) || (extension.Equals(".png")) || (extension.Equals(".jpg")) || (extension.Equals(".bmp")) || (extension.Equals(".gif")))
+            {
+                extension_flag = 1;
+            }
+            if (extension_flag == 0)
+            {
+                MessageBox.Show("Invalid File Format");
+            }
+            else
+            {
+                pic = new Bitmap(openFileDialog1.FileName);
+                im = pic;
+                pictureBox1.Image = im;
+                gfx = panel1.CreateGraphics();
+                width = pic.Width;
+                height = pic.Height;
+                matrix_red = new int[width, height];
+                matrix_green = new int[width, height];
+                matrix_blue = new int[width, height];
+                border_matrix = new int[width - 1, height - 1];
+                fill_matrix1 = new int[width - 1, height - 1];
+                fill_matrix2 = new int[width - 1, height - 1];
+                brush = new SolidBrush(Color.Black);
+                brush1 = new SolidBrush(Color.Blue);
+                penka = new Pen(brush);
+                penka1 = new Pen(brush1);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (extension_flag == 1)
+            {
+                for (int j = 0; j < height; j++)                       //Loop to make color matrices.
+                {
+                    for (int i = 0; i < width; i++)
+                    {
+                        pix = pic.GetPixel(i, j);
+                        matrix_red[i, j] = pix.R;
+                        matrix_green[i, j] = pix.G;
+                        matrix_blue[i, j] = pix.B;
+                    }
+                }
+
+                for (int j = 0; j < height - 1; j++)                      //matching here
+                {
+                    for (int i = 0; i < width - 1; i++)
+                    {
+                        Difference_value = 0;
+                        Difference_value += Math.Abs(matrix_red[i, j] - matrix_red[i + 1, j]);
+                        Difference_value += Math.Abs(matrix_green[i, j] - matrix_green[i + 1, j]);
+                        Difference_value += Math.Abs(matrix_blue[i, j] - matrix_blue[i + 1, j]);
+
+                        if (Difference_value >= threshold)
+                        {
+                            border_matrix[i, j] = 1;
+                        }
+                        else
+                        {
+                            border_matrix[i, j] = 0;
+                        }
+                    }
+                }
+                for (int i = 0; i < width - 1; i++)
+                {
+                    for (int j = 0; j < height - 1; j++)
+                    {
+                        Difference_value = 0;
+                        Difference_value += Math.Abs(matrix_red[i, j] - matrix_red[i, j + 1]);
+                        Difference_value += Math.Abs(matrix_green[i, j] - matrix_green[i, j + 1]);
+                        Difference_value += Math.Abs(matrix_blue[i, j] - matrix_blue[i, j + 1]);
+
+                        if (Difference_value >= threshold)
+                        {
+                            border_matrix[i, j] = 1;
+                        }
+                    }
+                }
+
+                for (int j = 0; j < height - 1; j++)
+                {
+                    for (int i = 0; i < width - 1; i++)
+                    {
+
+                       fill_matrix1[i, j] = border_matrix[i, j];
+                    }
+                }
+                
+                gfx.Clear(Color.White);
+
+
+                for (int j = 0; j < height - 1; j++)
+                {
+                    for (int i = 0; i < width - 1; i++)
+                    {
+
+                        if (border_matrix[i, j] == 1)
+                        {
+                            x = (float)i;
+                            y = (float)j;
+                            gfx.DrawRectangle(penka, x, y, 0.1f, 0.1f);
+                        }
+                    }
+                }
+            }
+        }
+        private void update(object sender, EventArgs e)
+        {
+            threshold = trackBar1.Value;
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            p = panel1.PointToClient(Cursor.Position);
+            //string str;
+            //str = p.X.ToString();
+            //str += ',';
+            //str += p.Y.ToString();
+            //MessageBox.Show(str);
+            bucketfill(p.X, p.Y);
+
+
+            for (int j = 0; j < height - 1; j++)
+            {
+                for (int i = 0; i < width - 1; i++)
+                {
+
+                    if (fill_matrix1[i, j] == 2)
+                    {
+                        x = (float)i;
+                        y = (float)j;
+                        gfx.DrawRectangle(penka1, x, y, 0.1f, 0.1f);
+                    }
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            p = pictureBox1.PointToClient(Cursor.Position);
+            for (int j = 0; j < height - 1; j++)
+            {
+                for (int i = 0; i < width - 1; i++)
+                {
+                    fill_matrix2[i, j] = border_matrix[i, j];
+                }
+            }
+            
+            bucketfill_twin(p.X, p.Y);
+
+            gfx.Clear(Color.White);
+            for (int j = 0; j < height - 1; j++)
+            {
+                for (int i = 0; i < width - 1; i++)
+                {
+
+                    if (fill_matrix2[i, j] == 2)
+                    {
+                        x = (float)i;
+                        y = (float)j;
+                        gfx.DrawRectangle(penka1, x, y, 0.1f, 0.1f);
+                    }
+                }
+            }
+        }
+    }
+}
